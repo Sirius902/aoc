43343140722ce6d55ce5cdf8dcbf77541a993014
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-05-31 08:50:29 -0700

    Add lf module to unit tests

--- build.zig
+++ build.zig
@@ -49,6 +49,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    unit_tests.addModule("lf", lf_mod);
+
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
