5a71298a5bc56e3e24240814cbdc1ae9d4e9938d
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-05-31 22:04:10 -0700

    Start working on day 3

---
+++ 2022/d03/src/main.zig
@@ -0,0 +1,70 @@
+const std = @import("std");
+const io = std.io;
+const mem = std.mem;
+const testing = std.testing;
+const lfStream = @import("lf").lfStream;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const stdin = io.getStdIn().reader();
+    std.log.info("{}", .{try sumOfPriorities(allocator, stdin)});
+}
+
+fn priority(c: u8) !usize {
+    return switch (c) {
+        'a'...'z' => c - 'a' + 1,
+        'A'...'Z' => c - 'A' + 27,
+        else => return error.InvalidChar,
+    };
+}
+
+fn sumOfPriorities(allocator: mem.Allocator, reader: anytype) !usize {
+    var stream = lfStream(reader);
+    const lf_reader = stream.reader();
+
+    var seen = std.AutoHashMap(u8, void).init(allocator);
+    defer seen.deinit();
+
+    var sum: usize = 0;
+
+    while (try lf_reader.readUntilDelimiterOrEofAlloc(
+        allocator,
+        '\n',
+        std.math.maxInt(usize),
+    )) |line| {
+        defer allocator.free(line);
+
+        const first = line[0 .. line.len / 2];
+        const second = line[line.len / 2 ..];
+
+        for (first) |c| {
+            if (!seen.contains(c) and std.mem.indexOfScalar(u8, second, c) != null) {
+                try seen.put(c, {});
+                sum += try priority(c);
+            }
+        }
+    }
+
+    return sum;
+}
+
+test "provided test" {
+    const input =
+        \\vJrwpWtwJgWrhcsFMMfFFhFp
+        \\jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        \\PmmdzqPrVvPwwTWBwg
+        \\wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        \\ttgJtRGJQctTZtZT
+        \\CrZsJsPPZsGzwwsLwLmpwMDw
+        \\
+    ;
+
+    var stream = io.fixedBufferStream(input);
+    try testing.expectEqual(
+        @as(usize, 157),
+        try sumOfPriorities(testing.allocator, stream.reader()),
+    );
+}
